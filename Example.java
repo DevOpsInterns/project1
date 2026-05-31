c261da0743b60cca56794ae4cd60fa2a360dd683
SravaniTechnologies <sravanitechworld@gmail.com>
2026-05-31 16:30:44 +0530

    first commit

---
+++ Example.java
@@ -0,0 +1,11 @@
+public class Example
+{
+public static void main(String[] args)
+{
+System.out.println("Hello world");
+}
+}
+/* This program is written by 
+Prashanth Kusuma
+For any other details please reach out to 
+projects2488@gmail.com */
