8ed19d76b76eb7fdc2306d1c668c8d3c4bf16018
prashanth <projects2488@gmail.com>
2026-05-31 11:05:16 +0000

    new commit

---
+++ Calc.java
@@ -0,0 +1,16 @@
+public class Example
+{
+public static void main(String[] args)
+{
+System.out.println("Calculator Menu");
+System.out.println("Addition: ");
+System.out.println("Subtraction: ");
+System.out.println("Multiplication: ");
+System.out.println("Division: ");
+System.out.println("Modulus: ");
+}
+}
+/* This program is written by 
+Arun
+For any other details please reach out to 
+projects2488@gmail.com */
